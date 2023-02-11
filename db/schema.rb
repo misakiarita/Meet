b91b4ab27669450cb9284eccbc4ec10f9dbe7921
misaki arita <misaki.arita0838@gmail.com>
2023-02-11 17:30:18 +0900

    Petの投稿を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_152727) do
+ActiveRecord::Schema.define(version: 2023_02_11_055731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "pet_name"
+    t.string "pet_address"
+    t.integer "qualify_age"
+    t.integer "status"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,4 +50,5 @@ ActiveRecord::Schema.define(version: 2023_02_10_152727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users"
 end
