875e81e352be491935460594d2a4cdd6f700f725
misaki arita <misaki.arita0838@gmail.com>
2023-02-15 22:39:46 +0900

    ログインエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   root to: 'pets#index'
+  
+  devise_for :users
   resources :users do
     member do
-      get :favorite_pet
+        get :favorite_pet
     end
   end
+
   resources :pets do
     resources :features
     patch :feature
     get :featurepost
   end
-  devise_for :users
+
   resources :favorites, only: [:create, :destroy]
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
