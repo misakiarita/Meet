fcdd25f82894a170001079ea56b89bd0a2a2246b
misaki arita <misaki.arita0838@gmail.com>
2023-02-22 23:41:10 +0900

    ユーザー新規登録スペック完了

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -3,16 +3,15 @@ RSpec.describe 'ユーザー登録機能', type: :system do
   describe '一般ユーザ登録のテスト' do
     context '一般ユーザー新規作成した場合' do
       it 'プロフィール画面にアクセスできる' do
-        visit new_user_path
+        visit new_user_registration_path
         fill_in 'user[name]', with: 'user1'
         fill_in 'user[email]', with: 'user1@gmail.com'
         fill_in 'user[password]', with: 'user1@gmail.com'
         fill_in 'user[password_confirmation]', with: 'user1@gmail.com'
-        fill_in 'user[address]', with: 'user1@gmail.com'
+        select '北海道', from: 'user_address'
         fill_in 'user[user_age]', with: '20'
         click_on '新規登録'
-        click_on '新規登録'
-
+        click_on 'プロフィール'
         expect(page).to have_content 'user1'
       end
     end 
