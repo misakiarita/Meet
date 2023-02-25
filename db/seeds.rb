4e41314b238ed8593ed537f289f8db5f0db07320
misaki arita <misaki.arita0838@gmail.com>
2023-02-26 02:20:20 +0900

    herokuへ再デプロイ

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # usersテーブルのseed
-# Devise.secret_key = '1614c111862e08e5049e3a701a39ed68f5e8fa8312e5502dcd0e2b18f9c35f5a3fe0e06dcf42025b19f001acb7d4d26925b99e1264c289c5337120222da13eb6'
-# User.create!(name: '田中', email: 'seed1@gmail.com', password: 'seed1@gmail.com', 
-#   password_confirmation: 'seed1@gmail.com', role: 3, address: 3, user_age: 20)
-# User.create!(name: '吉野', email: 'seed2@gmail.com', password: 'seed1@gmail.com', 
-#   password_confirmation: 'seed1@gmail.com', role: 2, address: 4, user_age: 30)
-# User.create!(name: '紫宮', email: 'seed3@gmail.com', password: 'seed1@gmail.com', 
-#   password_confirmation: 'seed1@gmail.com', role: 2, address: 4, user_age: 40)
-# User.create!(name: '額田', email: 'seed4@gmail.com', password: 'seed1@gmail.com', 
-#   password_confirmation: 'seed1@gmail.com', role: 1, address: 4, user_age: 50)
-# User.create!(name: '八代', email: 'seed5@gmail.com', password: 'seed1@gmail.com', 
-  # password_confirmation: 'seed1@gmail.com', role: 1, address: 4, user_age: 60)
+Devise.secret_key = '1614c111862e08e5049e3a701a39ed68f5e8fa8312e5502dcd0e2b18f9c35f5a3fe0e06dcf42025b19f001acb7d4d26925b99e1264c289c5337120222da13eb6'
+User.create!(name: '田中', email: 'seed1@gmail.com', password: 'seed1@gmail.com', 
+  password_confirmation: 'seed1@gmail.com', role: 3, address: 3, user_age: 20)
+User.create!(name: '吉野', email: 'seed2@gmail.com', password: 'seed1@gmail.com', 
+  password_confirmation: 'seed1@gmail.com', role: 2, address: 4, user_age: 30)
+User.create!(name: '紫宮', email: 'seed3@gmail.com', password: 'seed1@gmail.com', 
+  password_confirmation: 'seed1@gmail.com', role: 2, address: 4, user_age: 40)
+User.create!(name: '額田', email: 'seed4@gmail.com', password: 'seed1@gmail.com', 
+  password_confirmation: 'seed1@gmail.com', role: 1, address: 4, user_age: 50)
+User.create!(name: '八代', email: 'seed5@gmail.com', password: 'seed1@gmail.com', 
+  password_confirmation: 'seed1@gmail.com', role: 1, address: 4, user_age: 60)
 
 # 団体用データ
 organization = User.create!(name: '小森', email: 'seed10@gmail.com', password: 'seed1@gmail.com', 
