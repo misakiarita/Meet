4df771067b088ad20e27d063d8c98df0e565560c
misaki arita <misaki.arita0838@gmail.com>
2023-02-25 23:22:04 +0900

    userモデルテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,47 @@
 require 'rails_helper'
-RSpec.describe 'ユーザーモデル', type: :model do
+RSpec.describe 'userモデル', type: :model do
   describe 'バリデーションのテスト' do
-    context 'ユーザーが空の場合' do
+    context 'nameが空の場合' do
       it 'バリデーションにひっかる' do
-        feature = Feature.create(dog_or_cat:'', color: '失敗テスト', weight:'3', pet_age:'3', sex:1, pet_id: @pet.id)
-        expect(feature).not_to be_valid
+        user = User.create(name:'', email: 'tanaka@1.com', password:'tanaka@1.com', role:1, address:1, user_age:20 )
+        expect(user).not_to be_valid
       end
     end
 
+    context 'emailが空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.create(name:'たなか', email: '', password:'tanaka@1.com', role:1, address:1, user_age:20 )
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'roleが空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.create(name:'たなか', email: 'tanaka@1.com', password:'tanaka@1.com', role:'', address:1, user_age:20 )
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'addressが空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.create(name:'たなか', email: 'tanaka@1.com', password:'tanaka@1.com', role:1, address:'', user_age:20 )
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'user_ageが空の場合' do
+      it 'バリデーションにひっかる' do
+        user = User.create(name:'たなか', email: 'tanaka@1.com', password:'tanaka@1.com', role:1, address:1, user_age:'' )
+        expect(user).not_to be_valid
+      end
+    end
 
     context 'ユーザーが記載されているの場合' do
       it 'バリデーションにひっかからない' do
-        second_feature = Feature.create(dog_or_cat: 2, color: '黒', weight:'3', pet_age:'3', sex:1, pet_id: @pet.id)
-        expect(second_feature).to be_valid
+        user = User.create(name:'たなか', email: 'tanaka@1.com', password:'tanaka@1.com', role:1, address:1, user_age:'' )
+        expect(user).to be_valid
       end
     end
 
   end
 end
-
-t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.integer "address", null: false
-    t.text "avatar"
-    t.integer "role", null: false
-    t.text "profile"
-    t.integer "user_age", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
