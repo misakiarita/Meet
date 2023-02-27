5e3898a32856d25b03ca991ec1d198a75685c140
misaki arita <misaki.arita0838@gmail.com>
2023-02-27 11:13:11 +0900

    ペット、ユーザーの年齢の最小値制限

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,8 +1,8 @@
 class Feature < ApplicationRecord
   validates :dog_or_cat, presence: true
   validates :color, presence: true
-  validates :weight, presence: true
-  validates :pet_age, presence: true
+  validates :weight, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :pet_age, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :sex, presence: true
   belongs_to :pet
   enum dog_or_cat:{"犬":1, "猫":2}
