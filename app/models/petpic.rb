dad77fa8cdffc8c313abc0ccf687e09342aaef6b
misaki arita <misaki.arita0838@gmail.com>
2023-03-06 18:23:36 +0900

    一覧表示の画像を指定

--- app/models/petpic.rb
+++ app/models/petpic.rb
@@ -1,4 +1,8 @@
 class Petpic < ApplicationRecord
-  belongs_to :pet
   mount_uploader :picture, ImageUploader   
+  belongs_to :pet
+  # version :thumb do
+  #   process resize_to_limit: [100, 100]
+  #   default_url "/images/cat_mike.png" # デフォルトの画像URLを指定
+  # end
 end
